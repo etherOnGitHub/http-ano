fd4c370eb26cfde53e025ab14408268fcdf1c0b6
djpri <22898729+djpri@users.noreply.github.com>
2025-08-18 15:58:14 +0100

    Add comment to playSound function

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -1,7 +1,9 @@
-export function playSound(sound) {
+// example: playSound("assets/audio/piano/C5piano.mp3");
+
+export function playSound(soundFilePath) {
   // Play audio if available for this note (allow overlapping sounds)
-  if (sound) {
-    const audio = new Audio(sound);
+  if (soundFilePath) {
+    const audio = new Audio(soundFilePath);
     audio.currentTime = 0;
     audio.play();
   }
